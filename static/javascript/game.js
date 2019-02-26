cc2665750c64323bd07124d01b134c50bc044c5c
MateMiklos <miklos.mate.91@gmail.com>
2019-02-26 22:57:20 +0100

    the frog is ready to move

--- static/javascript/game.js
+++ static/javascript/game.js
@@ -2,7 +2,7 @@
 //     location.href = "game";
 // };
 
-let createCell = function(classname){
+let createCell = function (classname) {
     return `<div class="${classname}"></div>`;
 };
 
@@ -12,7 +12,10 @@ const waterElement = createCell('game-cell-water');
 const shoreElement = createCell('game-cell-shore');
 const sideRoadElement = createCell('game-cell-road');
 const MainRoadElement = createCell('game-cell-road2');
-const frog = document.getElementById('frog')
+const frog = document.getElementById('frog');
+const gameField = document.getElementById('game-field');
+var verticalPosition = 55;
+var horisontalPosition = 300;
 
 
 function createMapRow(type, length, j) {
@@ -22,17 +25,17 @@ function createMapRow(type, length, j) {
 }
 
 function createUpperRow(length) {
-    for (let i = 1; i < length+1; i++) {
-         if (i % 2 === 0) {
-          document.getElementById('upperRow').innerHTML += swampElement;
-         }
+    for (let i = 1; i < length + 1; i++) {
+        if (i % 2 === 0) {
+            document.getElementById('upperRow').innerHTML += swampElement;
+        }
         if (i % 2 === 1) {
-          document.getElementById('upperRow').innerHTML += grassElement;
-         }
+            document.getElementById('upperRow').innerHTML += grassElement;
+        }
     }
 }
 
-function createMap () {
+function createMap() {
 
     createUpperRow(11);
     for (let j = 0; j < 5; j++) {
@@ -55,7 +58,84 @@ function createMap () {
     }
 }
 
-createMap()
+
+createMap();
+
+
+// ################################################ MOVEMENT FEATURE ################################################ //
+
+
+function move(event) {
+    if (event.which === 38) {
+        verticalPosition += 50;
+        if (verticalPosition >= 650) {
+            verticalPosition -= 50
+        }
+        frog.style.bottom = verticalPosition + "px";
+    }
+    else if (event.which === 40) {
+        verticalPosition -= 50;
+        if (verticalPosition <= 5) {
+            verticalPosition += 50
+        }
+        frog.style.bottom = verticalPosition + "px";
+    }
+    else if (event.which === 37) {
+        horisontalPosition+= 50;
+        if (horisontalPosition >= 600) {
+            horisontalPosition -= 50
+        }
+        frog.style.right = horisontalPosition + "px";
+    }
+    else if (event.which === 39) {
+        horisontalPosition-= 50;
+        if (horisontalPosition <= 0) {
+            horisontalPosition += 50
+        }
+        frog.style.right = horisontalPosition + "px";
+    }
+}
+
+
+document.onkeydown = move;
+
+
+// function move(event) {
+//     if (event.which === 13) {
+//         alert('MOVE!!!')
+//     }
+//     let pos = 0;
+//     const frog = document.getElementById('frog');
+//     if (event.which === 38) {
+//         pos ++;
+//         frog.style.bottom = pos + "px"
+//     }
+// }
+
+
+// function move(event) {
+//     let verticalPosition = 55;
+//     let horisontalPosition = 300;
+//     let verticalStop = 105;
+//     let horisontalStop = 350;
+//     let id = setInterval(frame, 0);
+//     function frame() {
+//         if (verticalPosition === verticalStop || horisontalPosition === horisontalStop) {
+//             clearInterval(id);
+//         }
+//         else if (event.which === 38) {
+//             verticalPosition += 50;
+//             frog.style.bottom = verticalPosition + "px";
+//             // verticalPosition = verticalStop;
+//             // verticalStop += 50
+//         }
+//         else if (event.which === 37) {
+//             horisontalPosition++;
+//             frog.style.right = horisontalPosition + "px";
+//         }
+//
+//     }
+// }
 
 
 
