2f7efcf8c16d62feb80176b604f30c735da86eb4
Dávid Szabó <drszabodavid@gmail.com>
2019-02-26 17:40:06 +0100

    moving logs added

--- static/javascript/game.js
+++ static/javascript/game.js
@@ -1,19 +1,15 @@
-// document.getElementById("myButton").onclick = function () {
-//     location.href = "game";
-// };
 
-let createCell = function(classname){
-    return `<div class="${classname}"></div>`;
+let createCell = function (classname) {
+        return `<div class="${classname}" ></div>`
 };
-
 const grassElement = createCell('game-cell-grass');
 const swampElement = createCell('game-cell-swamp');
 const waterElement = createCell('game-cell-water');
 const shoreElement = createCell('game-cell-shore');
 const sideRoadElement = createCell('game-cell-road');
 const MainRoadElement = createCell('game-cell-road2');
-const frog = document.getElementById('frog')
-
+const frog = document.getElementById('frog');
+const ThreeLongLog = document.getElementById('ThreeLongLog');
 
 function createMapRow(type, length, j) {
     for (let i = 0; i < length; i++) {
@@ -22,18 +18,17 @@ function createMapRow(type, length, j) {
 }
 
 function createUpperRow(length) {
-    for (let i = 1; i < length+1; i++) {
-         if (i % 2 === 0) {
-          document.getElementById('upperRow').innerHTML += swampElement;
-         }
+    for (let i = 1; i < length + 1; i++) {
+        if (i % 2 === 0) {
+            document.getElementById('upperRow').innerHTML += swampElement;
+        }
         if (i % 2 === 1) {
-          document.getElementById('upperRow').innerHTML += grassElement;
-         }
+            document.getElementById('upperRow').innerHTML += grassElement;
+        }
     }
 }
 
-function createMap () {
-
+function createMap() {
     createUpperRow(11);
     for (let j = 0; j < 5; j++) {
         createMapRow(waterElement, 11, j);
@@ -55,7 +50,50 @@ function createMap () {
     }
 }
 
-createMap()
+function LogMove(logname, startposition, speed) {
+  var elem = document.getElementById(logname);
+  var pos = startposition;
+  var id = setInterval(frame, speed);
+  function frame() {
+    if (pos === 550) {
+      clearInterval(id);
+    } else {
+      pos++;
+      elem.style.right = pos + "px";
+      if (pos === 550)
+          LogMove(logname, startposition, speed)
+    }
+  }
+}
+
+function LogMoveReversed(logname, startposition, speed) {
+  var elem = document.getElementById(logname);
+  var pos = startposition;
+  var id = setInterval(frame, speed);
+  function frame() {
+    if (pos === 700) {
+      clearInterval(id);
+    } else {
+      pos++;
+      elem.style.left = pos + "px";
+      if (pos === 700)
+          LogMoveReversed(logname, startposition, speed)
+    }
+  }
+}
+
+createMap();
+
+
+
+LogMove("ThreeLongLog", -900, 12);
+LogMove("ThreeLongLog2", -450, 12);
+LogMove("ThreeLongLog3", -200, 12);
+LogMove("FourLongLog", -200, 15, );
+LogMove("FourLongLog2", -700, 15, );
+LogMoveReversed("ThreeLongLogReversed", -300, 10)
+LogMoveReversed("ThreeLongLogReversed2", -600, 10)
+
 
 
 
