d4390d619917da047c150aef759bc505412fc3a8
ZalanLP <zalan.permenter@gmail.com>
2019-02-27 15:03:21 +0100

    Timer for Frogger game complete, ready to implement restart upon win

--- static/javascript/game.js
+++ static/javascript/game.js
@@ -139,5 +139,31 @@ function move(event) {
 document.onkeydown = move;
 
 
+var h1 = document.getElementsByTagName('h1')[0],
+//    clear = document.getElementById('clear'),
+    seconds = 0, minutes = 0, hours = 0,
+    t;
+
+function add() {
+    seconds++;
+    if (seconds >= 60) {
+        seconds = 0;
+        minutes++;
+        if (minutes >= 60) {
+            minutes = 0;
+        }
+    }
+
+    h1.textContent = (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "0") + ":" + (seconds > 9 ? seconds : "0" + seconds);
+
+    timer();
+}
+function timer() {
+    t = setTimeout(add, 1000);
+}
+timer();
+
+
+
 
 
